0a9192069b77218770cd53cc269a01c2a1091299
songmilee <lsm941010@gmail.com>
2024-05-23 21:12:01 +0900

    Initial Commit

---
+++ BookBaram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookBaram
+//
+//  Created by 이송미 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# 북바람
+
+## 앱의 목적
+
+- 본인이 읽은 도서에 대한 감상평을 앱을 통해 간단히 남기도록 합니다.
+
+## 와이어 프레임
+
+- https://app.moqups.com/yPALgQU0DIck8BdvDY6w3tcgFirvAUlg/view/page/aa291aa6c
