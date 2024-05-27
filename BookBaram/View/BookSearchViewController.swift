a02428f5df4262452c6458c2b0952bf597aa899c
songmilee <lsm941010@gmail.com>
2024-05-27 21:40:07 +0900

    feat: 책 검색 용 ViewController 추가 및 책 검색용 스토리보드 구성

---
+++ BookBaram/View/BookSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookSearchViewController.swift
+//  BookBaram
+//
+//  Created by 이송미 on 5/27/24.
+//
+
+import UIKit
+
+class BookSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
