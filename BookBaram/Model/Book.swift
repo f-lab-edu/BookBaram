f0b8c6b3292b9672a78d4bea55f5a0eab4d8f712
songmilee <lsm941010@gmail.com>
2024-05-27 22:29:25 +0900

    feat: 책 검색 - 책 모델 추가

---
+++ BookBaram/Model/Book.swift
@@ -0,0 +1,33 @@
+//
+//  Book.swift
+//  BookBaram
+//
+//  Created by 이송미 on 5/27/24.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - SearchBookResponse
+struct SearchBookResponse: Codable {
+    let total: Int
+    let start: Int
+    let display: Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let title: String
+    let link: URL
+    let image: URL
+    let author: String
+    let discount: String
+    let publisher: String
+    let pubdate: Date
+    let isbn: Int
+    let description: String
+    
+    
+}
+
