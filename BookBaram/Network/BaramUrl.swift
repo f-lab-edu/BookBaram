a1ae93f253307059061faff6306e88fcc8a1b967
songmilee <lsm941010@gmail.com>
2024-06-01 15:57:20 +0900

    feat: 네트워크 - Api 요청을 위한 url 경로 및 에러 추가

---
+++ BookBaram/Network/BaramUrl.swift
@@ -0,0 +1,16 @@
+//
+//  BaramUrl.swift
+//  BookBaram
+//
+//  Created by 이송미 on 6/1/24.
+//
+
+import Foundation
+
+struct BaramUrl {
+    public static let naverApi = "https://openapi.naver.com/"
+}
+
+struct Path {
+    public static let bookSearch = "/search/book.json"
+}
