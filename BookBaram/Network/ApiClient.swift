f17d1492bff7c5cc93425007093c167c96925c6a
songmilee <lsm941010@gmail.com>
2024-06-01 16:16:02 +0900

    feat: 네트워크 - post 요청 관련 메소드 삭제 및 성공 시 Data non-null 리턴으로 변경

--- BookBaram/Network/ApiClient.swift
+++ BookBaram/Network/ApiClient.swift
@@ -7,32 +7,36 @@
 
 import Foundation
 
-enum HTTPMethod: String {
-    case Get, Post
-}
-
 struct ApiClient {
     public static let shared = ApiClient()
     private let session = URLSession.shared
     
     func request(_ urlString: String,
-                 method: HTTPMethod,
                  parameters: [String: Any]? = nil,
                  headers: [String: String]? = nil,
-                 completion: @escaping (Result<Data?, Error>) -> Void) {
+                 completion: @escaping (Result<Data, Error>) -> Void) {
         
-        var urlRequest: URLRequest? = nil
-        
-        if method == .Get {
-            urlRequest = createGetRequest(urlString, parameters: parameters)
-        } else if method == .Post {
-            urlRequest = createPostRequest(urlString, parameters: parameters)
-        }
-        
-        guard var urlRequest else {
+        guard var urlComponent = URLComponents(string: urlString) else {
             completion(.failure(BaramErrorInfo(error: .InvalidRequest)))
             return
         }
+        
+        if let parameters {
+            var queryItems = urlComponent.queryItems ?? []
+            for param in parameters {
+                queryItems.append(URLQueryItem(name: param.key, value: "\(param.value)"))
+            }
+            
+            urlComponent.queryItems = queryItems
+        }
+        
+        guard let url = urlComponent.url else {
+            completion(.failure(BaramErrorInfo(error: .InvalidRequest)))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "get"
         
         if let headers {
             for i in headers {
@@ -47,7 +51,7 @@ struct ApiClient {
                 return
             }
                         
-            guard let httpResponse = response as? HTTPURLResponse, (200..<300) ~= httpResponse.statusCode else {
+            guard let data, let httpResponse = response as? HTTPURLResponse, (200..<300) ~= httpResponse.statusCode else {
                 completion(.failure(BaramErrorInfo(error: .InvalidResponse)))
                 return
             }
@@ -55,40 +59,5 @@ struct ApiClient {
             completion(.success(data))
         }
         .resume()
-    }
-}
-
-// MARK: - Internal Method
-extension ApiClient {
-    private func createGetRequest(_ urlString: String, parameters: [String: Any]?) -> URLRequest? {
-        guard let parameters, var urlComponent = URLComponents(string: urlString) else { return nil }
-        
-        var queryItems = urlComponent.queryItems ?? []
-        for param in parameters {
-            queryItems.append(URLQueryItem(name: param.key, value: "\(param.value)"))
-        }
-        
-        urlComponent.queryItems = queryItems
-        
-        if let url = urlComponent.url {
-            var urlRequest = URLRequest(url: url)
-            urlRequest.httpMethod = HTTPMethod.Get.rawValue
-            return urlRequest
-        }
-        
-        return nil
-    }
-    
-    private func createPostRequest(_ urlString: String, parameters: [String: Any]?) -> URLRequest? {
-        guard let url = URL(string: urlString) else { return nil }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = HTTPMethod.Post.rawValue
-        
-        if let parameters {
-            urlRequest.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-        }
-        
-        return urlRequest
     }
 }
