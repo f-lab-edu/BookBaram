3511f269e34457d8cd457b08d983c39322c113c4
songmilee <lsm941010@gmail.com>
2024-06-30 23:29:01 +0900

    feat: dynamic type - 누락된 뷰에 다이나믹 타입 추가

--- BookBaram/View/Edit/EditView.swift
+++ BookBaram/View/Edit/EditView.swift
@@ -71,13 +71,15 @@ class EditView: UIView {
     }
 
     private func textFieldStyle() {
-        titleTextField.dynamicFont()
+        titleTextField.font = UIFont.preferredFont(forTextStyle: .body)
+        titleTextField.adjustsFontForContentSizeCategory = true
         titleTextField.borderStyle = .roundedRect
         titleTextField.placeholder = "Write the title"
     }
 
     private func contentTextViewStyle() {
-        contentTextView.dynamicFont()
+        contentTextView.font = UIFont.preferredFont(forTextStyle: .body)
+        contentTextView.adjustsFontForContentSizeCategory = true
         contentTextView.layer.borderWidth = 1.0
         contentTextView.layer.cornerRadius = 8
         contentTextView.layer.borderColor = UIColor.lightGray.cgColor
