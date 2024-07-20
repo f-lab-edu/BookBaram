562d8ba1701ef0dc0a7547661cd5f2ef5ac1aeda
songmilee <lsm941010@gmail.com>
2024-07-20 16:46:27 +0900

    feat: 이미지 캐시 - 디스크 캐시 추가

--- BookBaram/Utils/Image/ImageCache.swift
+++ BookBaram/Utils/Image/ImageCache.swift
@@ -12,12 +12,17 @@ final class ImageCache {
     public static let shared = ImageCache()
 
     private let memoryCache = NSCache<NSString, UIImage>()
+    private let diskCache = ImageDiskCache()
 
     private init() { }
 
     func loadImage(imageUrl key: String) async -> UIImage? {
-        if let result = memoryCache.object(forKey: key as NSString) {
-            return result
+        if let memoryResult = memoryCache.object(forKey: key as NSString) {
+            return memoryResult
+        }
+
+        if let diskResult = try? diskCache.loadImage(imageUrl: key) {
+            return UIImage(data: diskResult)
         }
 
         return await downloadImage(imageUrl: key)
@@ -27,6 +32,7 @@ final class ImageCache {
         if let imgData = try? await ImageDownloader.shared.downloadImage(imageUrl: imageUrl),
            let image = UIImage(data: imgData) {
             memoryCache.setObject(image, forKey: imageUrl as NSString)
+            diskCache.saveImage(imageUrl: imageUrl, image: imgData)
             return image
         }
 
--- BookBaram/Utils/Image/ImageDiskCache.swift
+++ BookBaram/Utils/Image/ImageDiskCache.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 final class ImageDiskCache {
+    private let cachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first
+    private let fileManager = FileManager()
 
+    func loadImage(imageUrl key: String) throws -> Data? {
+        guard let cachePath else { return nil }
+
+        let filePath = URL(filePath: cachePath).appending(path: key)
+
+        if !fileManager.fileExists(atPath: filePath.path()) {
+            return nil
+        }
+
+        return try Data(contentsOf: filePath)
+    }
+
+    func saveImage(imageUrl key: String, image data: Data) {
+        guard let cachePath else { return }
+
+        let filePath = URL(filePath: cachePath).appending(path: key)
+
+        _ = fileManager.createFile(atPath: filePath.path(), contents: data)
+    }
 }
